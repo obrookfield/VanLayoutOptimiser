c2f2286a4e5678f63d5d66bfc79375f9cb9a9732
Oliver Brookfield <oliverkbrookfield@gmail.com>
2026-07-16 21:00:09 +0100

    Fixed __init__ to include Osbtacle object.

--- van_layout/__init__.py
+++ van_layout/__init__.py
@@ -1,3 +1,3 @@
-from .geometry import Item, Van, overlaps, in_bounds
+from .geometry import Item, Van, Obstacle, overlaps, in_bounds
 
 __all__ = ["Item", "Van", "Obstacle", "overlaps", "in_bounds"]
